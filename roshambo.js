89db642f61e58f6220fbc810f60cee48996e6219
Ivan-Jensen <ivanjen99@gmail.com>
2025-08-07 20:02:27 -0700

    Add getComputerChoice function description
    Add a description for the getComputerChoice function so that it's clear
    to myself and other readers what the function does, its parameters, and
    its return values.

--- roshambo.js
+++ roshambo.js
@@ -3,16 +3,31 @@
  * Ivan Jensen
  * 08/07/2025
  *
- * This program lets the user play rock paper scissors against the computer.
- * The game is played in rounds and will prompt the player to pick rock, 
- * paper, or scissors (case insensitive) at which point the computer will 
- * pick a random option (rock, paper, or scissors) and will tell the player 
- * who won the round. The program will run as long as the player wants, with
- * the option to quit after each round. Additionally, the program will keep
- * track of the total scores of the player and the computer as well as the 
- * current round number. The game will only run on the console, but it may be
- * given a GUI in the future. 
+ * This program lets the user play rock paper scissors against the 
+ * computer. The game is played in rounds and will prompt the player to
+ * pick rock, paper, or scissors (case insensitive) at which point the
+ * computer will pick a random option (rock, paper, or scissors) and
+ * will tell the player who won the round. The program will run as long
+ * as the player wants with the option to quit after each round
+ * Additionally, the program will keep track of the total scores of the
+ * player and the computer as well as the current round number. The
+ * game will only run on the console, but it may be given a GUI in the
+ * future. 
  */
 
 
-
+/* 
+ * getComputerChoice
+ * 
+ * Has the computer choose options rock, paper, or scissors to play
+ * against the player.
+ * 
+ * Parameters:
+ *     None
+ * 
+ * Returns:
+ *     "rock":     If the computer chooses rock
+ *     "paper":    If the computer chooses paper
+ *     "scissors": If the computer chooses scissors
+ *
+ */
