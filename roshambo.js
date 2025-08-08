f14b6d45a01ce46f2abce8b315839d5cfb10b36b
Ivan-Jensen <ivanjen99@gmail.com>
2025-08-07 19:37:10 -0700

    Create roshambo.js for rock-paper-scissors game
    This file implements the logic for a simple rock-paper-scissors game
    where a given user can play against the computer. The game is played
    entirely in the console.

---
+++ roshambo.js
@@ -0,0 +1,18 @@
+/*
+ * roshambo.js
+ * Ivan Jensen
+ * 08/07/2025
+ *
+ * This program lets the user play rock paper scissors against the computer.
+ * The game is played in rounds and will prompt the player to pick rock, 
+ * paper, or scissors (case insensitive) at which point the computer will 
+ * pick a random option (rock, paper, or scissors) and will tell the player 
+ * who won the round. The program will run as long as the player wants, with
+ * the option to quit after each round. Additionally, the program will keep
+ * track of the total scores of the player and the computer as well as the 
+ * current round number. The game will only run on the console, but it may be
+ * given a GUI in the future. 
+ */
+
+
+
